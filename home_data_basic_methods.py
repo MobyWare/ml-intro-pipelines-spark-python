d9f0d84b9808e26ef8d3e781bf1098476e1ecba5
Dick <dickm@upmc.edu>
2016-12-12 11:18:50 -0500

    added spark 2.0 docs & initial regression

--- home_data_basic_methods.py
+++ home_data_basic_methods.py
@@ -1,9 +1,50 @@
-from numpy import genfromtxt
 import pyspark_setup # Need to import pre-requisits set up by pyspark.
-from pyspark import SparkContext, SparkConf
-csv_data = genfromtxt('home_data.csv', delimiter=',', names=True)
+from pyspark.sql import SparkSession
 
-sc = SparkContext('local[3]')
-homeRDD = sc.parallelize(csv_data)
-print(homeRDD.count())
-print(homeRDD.take(2))
+spark = SparkSession\
+    .builder\
+    .appName("ML_Basic_Home_Data")\
+    .getOrCreate()
+
+from pyspark.sql.types import DoubleType, IntegerType
+from pyspark.ml.feature import VectorAssembler, OneHotEncoder, StringIndexer
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml import Pipeline
+from pyspark.ml.regression import GeneralizedLinearRegression
+
+# Data Munging
+
+df = spark.read.csv('home_data.csv', header=True)
+df = df.withColumn("price", df["price"].cast(DoubleType()))\
+                   .withColumn("sqft_living", df["sqft_living"].cast(DoubleType()))
+                
+#df.printSchema()
+
+# Get training sets
+
+(trainData, testData) = df.randomSplit(seed=123, weights=[0.7,0.3])
+print("The total data is {}, the training is {} and the test is {}"\
+      .format(df.count(), trainData.count(), testData.count()))
+
+#Train & Evaluate
+
+stringifier = StringIndexer(inputCol="zipcode", outputCol="zipIndex")
+oneHotter = OneHotEncoder(inputCol="zipIndex", outputCol="zipVector")
+vectorizer = VectorAssembler(inputCols=["sqft_living", "zipVector"], outputCol="features")
+glr = GeneralizedLinearRegression(labelCol="price", family="gaussian", link="identity", maxIter=10, regParam=0.3)
+simplePipeline = Pipeline(stages=[stringifier, oneHotter, vectorizer, glr])
+model = simplePipeline.fit(trainData)
+
+#Print Reslts
+
+#testingData = vectorizer.transform(testData)
+# Make predictions.
+predictions = model.transform(testData)
+
+# Select example rows to display.
+predictions.select("prediction", "price", "features").show(5)
+
+# Select (prediction, true label) and compute test error
+evaluator = RegressionEvaluator(labelCol="price", predictionCol="prediction", metricName="rmse")
+rmse = evaluator.evaluate(predictions)
+print("RMSE is: {}".format(rmse))
