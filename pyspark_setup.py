af170300b9722de7e7a858571ae053145ddab973
dickm <dickm@upmc.edu>
2016-12-15 18:27:39 -0500

    Fixed issue with reading file with env set

--- pyspark_setup.py
+++ pyspark_setup.py
@@ -5,6 +5,20 @@ import sys
 sys.path.append("%s\\python\\lib\\py4j-0.10.3-src.zip" % os.getenv('SPARK_HOME'))
 sys.path.append("%s\\python" % os.getenv('SPARK_HOME'))
 
+## Remove environment variables in my system that may mess with reading a local file in a python script
+
+# Hadoop configs mess with reading relative file paths
+if os.environ.get('HADOOP_CONF_DIR') is not None:
+    del os.environ['HADOOP_CONF_DIR']
+
+
+# Jupyter configs in my environment may mesh with running a python script. Removing PYSPARK_* assumes a default python interpreter in PATH
+if os.environ.get('PYSPARK_PYTHON') is not None:
+    del os.environ['PYSPARK_PYTHON']
+
+if os.environ.get('PYSPARK_DRIVER_PYTHON') is not None:
+    del os.environ['PYSPARK_DRIVER_PYTHON']
+
 try:
     import pyspark    
     print("Successfully imported Spark modules.")
