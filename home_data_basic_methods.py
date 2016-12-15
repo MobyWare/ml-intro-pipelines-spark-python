e5d8a5e3c035915411373fa50fe92c021f100311
Dick <dickm@upmc.edu>
2016-12-14 22:09:04 -0500

    Add loop and rf alg

--- home_data_basic_methods.py
+++ home_data_basic_methods.py
@@ -10,7 +10,7 @@ from pyspark.sql.types import DoubleType, IntegerType
 from pyspark.ml.feature import VectorAssembler, OneHotEncoder, StringIndexer
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml import Pipeline
-from pyspark.ml.regression import GeneralizedLinearRegression
+from pyspark.ml.regression import GeneralizedLinearRegression, RandomForestRegressor
 
 # Data Munging
 
@@ -32,19 +32,23 @@ stringifier = StringIndexer(inputCol="zipcode", outputCol="zipIndex")
 oneHotter = OneHotEncoder(inputCol="zipIndex", outputCol="zipVector")
 vectorizer = VectorAssembler(inputCols=["sqft_living", "zipVector"], outputCol="features")
 glr = GeneralizedLinearRegression(labelCol="price", family="gaussian", link="identity", maxIter=10, regParam=0.3)
-simplePipeline = Pipeline(stages=[stringifier, oneHotter, vectorizer, glr])
-model = simplePipeline.fit(trainData)
+rf = RandomForestRegressor(labelCol="price")
+rfAdv = RandomForestRegressor(labelCol="price", numTrees=100, maxMemoryInMB=512, maxDepth=15)
+for alg in [glr, rf, rfAdv]:
+    print("+++++Method Start+++++")
+    simplePipeline = Pipeline(stages=[stringifier, oneHotter, vectorizer, alg])
+    model = simplePipeline.fit(trainData)
 
-#Print Reslts
+    #Print Reslts
 
-#testingData = vectorizer.transform(testData)
-# Make predictions.
-predictions = model.transform(testData)
+    #testingData = vectorizer.transform(testData)
+    # Make predictions.
+    predictions = model.transform(testData)
 
-# Select example rows to display.
-predictions.select("prediction", "price", "features").show(5)
+    # Select example rows to display.
+    predictions.select("prediction", "price", "features").show(5)
 
-# Select (prediction, true label) and compute test error
-evaluator = RegressionEvaluator(labelCol="price", predictionCol="prediction", metricName="rmse")
-rmse = evaluator.evaluate(predictions)
-print("RMSE is: {}".format(rmse))
+    # Select (prediction, true label) and compute test error
+    evaluator = RegressionEvaluator(labelCol="price", predictionCol="prediction", metricName="rmse")
+    rmse = evaluator.evaluate(predictions)
+    print("RMSE is: {}".format(rmse))
